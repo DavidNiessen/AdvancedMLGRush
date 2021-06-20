be1d905cf9bc6005ef1b95442e83dba2349ed7b7
AbgeSkillt0 <david@hniessen.de>
2021-06-20 23:56:50 +0200

    sync

--- src/main/java/net/skillcode/advancedmlgrush/placeholder/Placeholders.java
+++ src/main/java/net/skillcode/advancedmlgrush/placeholder/Placeholders.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2021 SkillCode
+ *
+ * This class is a part of the source code of the
+ * AdvancedMLGRush plugin from SkillCode.
+ *
+ * This class may only be used in compliance with the
+ * LICENSE.txt (https://github.com/SkillC0de/AdvancedMLGRush/blob/master/LICENSE.txt).
+ *
+ * Support: https://discord.skillplugins.com
+ */
+
 package net.skillcode.advancedmlgrush.placeholder;
 
 import com.google.inject.Singleton;
